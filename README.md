60ca6326a719473aa268c5ac1f288f3321108f42
nervatura <kappel.csaba@gmail.com>
2019-09-19 23:45:24 +0300

    v.2.4.0

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# nervatura-client
 Nervatura Client
+=========================
+
+Free to use Nervatura Business Management Application
+
+## Installation & Quick Start
+
+    $ git clone https://github.com/nervatura/nervatura-express.git nervatura
+    $ cd nervatura
+    $ npm install
+
+Start the server
+
+* development mode:
+```
+  $ NODE_ENV=development node server.js
+```
+and [http://localhost:8080/client/](http://localhost:8080/client/)
+
+* production mode:
+```
+  $ npm start
+```
+and [https://localhost:8080/client/](https://localhost:8080/client/)
+
+## Docs & Community
+
+Please see more the [Nervatura Docs](https://nervatura.github.io/nervatura-docs)
+
+More info see http://nervatura.com.
---
+++ build/precache-manifest.ef7b683f5bdb6526c2a765493917ad7e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68e1883f34b44c2ff33dde06e826afd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a21e5565983d917a5fd6",
+    "url": "./static/css/2.14389db1.chunk.css"
+  },
+  {
+    "revision": "fef58b18355c0b11d230",
+    "url": "./static/css/main.1c58f58e.chunk.css"
+  },
+  {
+    "revision": "a21e5565983d917a5fd6",
+    "url": "./static/js/2.3f4b5347.chunk.js"
+  },
+  {
+    "revision": "fef58b18355c0b11d230",
+    "url": "./static/js/main.fb1a11ee.chunk.js"
+  },
+  {
+    "revision": "034bfb8a157473a56787",
+    "url": "./static/js/runtime~main.e11b2c6a.js"
+  },
+  {
+    "revision": "2f66ece08dd05540c8dcd9dbd7cc299b",
+    "url": "./static/media/droid-sans-v6-latin-700.2f66ece0.woff2"
+  },
+  {
+    "revision": "754295cd4a92681c642d4d0c64dc1327",
+    "url": "./static/media/droid-sans-v6-latin-700.754295cd.ttf"
+  },
+  {
+    "revision": "94bb58d7d784bdd0a2a969ae6cc10d82",
+    "url": "./static/media/droid-sans-v6-latin-700.94bb58d7.eot"
+  },
+  {
+    "revision": "ca009f5f566bbbd022b602c4305352df",
+    "url": "./static/media/droid-sans-v6-latin-700.ca009f5f.svg"
+  },
+  {
+    "revision": "e0a7f5275100bfd0641b76119ee25cf4",
+    "url": "./static/media/droid-sans-v6-latin-700.e0a7f527.woff"
+  },
+  {
+    "revision": "08856bbb66d70dae7ca1392921cdad3b",
+    "url": "./static/media/droid-sans-v6-latin-regular.08856bbb.ttf"
+  },
+  {
+    "revision": "0e58871b40805fd59efea39c0a2feaf5",
+    "url": "./static/media/droid-sans-v6-latin-regular.0e58871b.woff"
+  },
+  {
+    "revision": "1670b01a551f3a88d0c1f07f9bfba1ad",
+    "url": "./static/media/droid-sans-v6-latin-regular.1670b01a.svg"
+  },
+  {
+    "revision": "2cca3e29a25dfbfcd55d905b559d4830",
+    "url": "./static/media/droid-sans-v6-latin-regular.2cca3e29.eot"
+  },
+  {
+    "revision": "ae8b5d5f0ff2af63e4361fcb8d8fb949",
+    "url": "./static/media/droid-sans-v6-latin-regular.ae8b5d5f.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
